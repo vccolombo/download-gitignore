12cb72f8595a1e3678066f23c6f9c7d6b87f9f86
Víctor Colombo <victorcora@hotmail.com>
2019-09-17 13:26:37 -0300

    Create setup and requirements

---
+++ download_gitignore/download_gitignore.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello gitignore")
+    
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="download-gitignore",
+    version="0.1",
+    description="A Python CLI program to download .gitignore files from Github's repository",
+    url="https://github.com/vccolombo/download-gitignore",
+    author="Víctor Cora Colombo",
+    author_email="victorcora98@gmail.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "click==7.0",
+    ],
+    entry_points={
+        "console_scripts": ["dgi=download_gitignore.download_gitignore:main"],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
